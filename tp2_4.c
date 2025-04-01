b24e8fe3c2c4d6cba4045810d678814dfc37a871
slackku <bananuya3@gmail.com>
2025-04-01 16:56:33 -0300

    Desarrollo punto 3, temrinar

--- tp2_4.c
+++ tp2_4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define CANTIDAD 5
+
 struct compu
 {
     int velocidad;        // Velocidad de procesamiento en GHz (valor entre 1 y 3)
@@ -9,17 +11,21 @@ struct compu
 } typedef PC;
 
 PC crearComputadora(char *tipos);
-
+void listarPCs(PC listaCompus[], int cantidad);
 int main()
 {
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+    char *pTipos = tipos;
 
-    PC compu1 = crearComputadora(tipos);
-    PC compu2 = crearComputadora(tipos);
-    PC compu3 = crearComputadora(tipos);
-    PC compu4 = crearComputadora(tipos);
-    PC compu5 = crearComputadora(tipos);
+    PC compu1 = crearComputadora(pTipos);
+    PC compu2 = crearComputadora(pTipos);
+    PC compu3 = crearComputadora(pTipos);
+    PC compu4 = crearComputadora(pTipos);
+    PC compu5 = crearComputadora(pTipos);
 
+    PC listaCompus[CANTIDAD] = {compu1, compu2, compu3, compu4, compu5};
+
+    listarPCs(listaCompus, CANTIDAD);
 
     return 0;
 }
@@ -30,6 +36,21 @@ PC crearComputadora(char *tipos)
         rand() % (4 - 1 + 1) + 1,
         rand() % (2025 - 2015 + 1) + 2015,
         rand() % (9 - 1 + 1) + 1,
-        tipos[rand() % 6]};
+        tipos += rand() % (6 - 1 + 1) + 1};
     return computadora;
+}
+
+void listarPCs(PC listaCompus[], int cantidad)
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("Velocidad: %d\n", listaCompus[i].velocidad);
+        printf("Anio: %d\n", listaCompus[i].anio);
+        printf("Cantidad Nucleos: %d\n");
+        while (listaCompus[i].tipo_cpu != "\0")
+        {
+            printf("%c", listaCompus[i].tipo_cpu);
+            listaCompus[i].tipo_cpu++;
+        }
+    }
 }
